ef7a45a5c13b490a6da8a6c2c9907591510a9144
HJ-HAKIN <hakin1128hivelab@gmail.com>
2018-06-29 18:30:57 +0900

    ex6

--- exam6/js/main.js
+++ exam6/js/main.js
@@ -9,37 +9,59 @@ $(function() {
             this.tabWrap = $('.js-tab-wrap');
             this.tabMenu = this.tabWrap.find('.cast_tab ul');
             this.tabMenuChild = this.tabMenu.children();
+            this.tabMenuLink = this.tabMenuChild.find('a');
             this.tabCont = this.tabWrap.find('.cast_cont');
             this.tabContChild = this.tabCont.children();
             this.prevBth = this.tabWrap.find('.btn_prev');
             this.nextBth = this.tabWrap.find('.btn_next');
+            this.pageCount = this.tabWrap.find('.cast_num .current');
+            this.pageMax = this.tabWrap.find('.cast_num .total');
         },
         initLayout : function() {
             this.currentIndex = 0;
             this.oldIndex = this.currentIndex;
-            // this.tabMenuChild.removeClass('.active');
-            // this.tabMenuChild.eq(0).addClass('.active');
-            // this.tabContChild.hide();
-            // this.tabContChild.eq(0).show();
-            this.setArray();
-        },
-        setArray : function(){
-            //규칙 리셋 = 삭제한 인덱스 초기화
-            this.arrayIndex = [];
-            for(var i = 0; i < this.tabMenuChild.length; i++){
-                this.arrayIndex.push(i);
-                // i가 listChild의 갯수보다 작아지면 arrayIndex의 뒤에 i를 추가(리셋)한다.
-            };
+            this.tabMenuChild.eq(this.currentIndex).addClass('active');
+            this.tabContChild.eq(this.currentIndex).addClass('active');
+            this.countIndex();
         },
         bindEvent : function() {
+            this.tabMenuLink.on('click', $.proxy(this.btnTabFunc, this));
             this.prevBth.on('click', $.proxy(this.btnPrevFunc, this));
             this.nextBth.on('click', $.proxy(this.btnNextFunc, this));
+            $(window).on('hashChange', $.proxy(this.onHashChange, this));
         },
-        btnPrevFunc : function() {
+        btnTabFunc : function(e) {
+            var target = $(e.currentTarget);
+            var targetIndex = target.parent().index();
+            this.currentIndex = targetIndex;
+            this.findIndex();
+            this.countIndex();
+        },
+        findIndex : function() {
+            this.tabMenuChild.eq(this.oldIndex).removeClass('active');
+            this.tabContChild.eq(this.oldIndex).removeClass('active');
+            this.tabMenuChild.eq(this.currentIndex).addClass('active');
+            this.tabContChild.eq(this.currentIndex).addClass('active');
+            this.oldIndex = this.currentIndex;
+        },
+        countIndex : function() {
+            this.count = this.currentIndex + 1;
+            this.pageCount.text(this.count);
+            this.countMax = this.tabMenuChild.length;
+            this.pageMax.text(this.countMax);
+        },
+        btnPrevFunc : function(e) {
             
         },
-        btnNextFunc : function() {
+        btnNextFunc : function(e) {
 
+        },
+        onHashChange : function () {
+            // hash의 값이 바뀌기 때문에 누를 때마다 뜸
+        },
+        getHashFunc : function () {
+            var getHash = window.location.href;
+            window.location.href = 'e' + id;
         }
     };
     // var slideMenu = {
