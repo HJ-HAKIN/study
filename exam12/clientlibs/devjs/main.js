3a60be7d2b4152ade3f2ab6e93422306961aa7f8
HJ-HAKIN <hakin1128hivelab@gmail.com>
2018-08-21 18:46:48 +0900

    ex12 js

--- exam12/clientlibs/devjs/main.js
+++ exam12/clientlibs/devjs/main.js
@@ -7,8 +7,24 @@
         	activeClass : 'is-active',
         	personaWrap : '.manual-download-filter-new__persona',
         	personaBx : '.manual-download-filter-new__persona-box',
-        	selectPlaceholder : '.support-select__placeholder',
+        	searchWrap : '.js-inptext-wrap',
+        	inpClear : '.support-input__clear',
+        	selectWrap : '.js-select-wrap',
+        	selectBtn : '.support-select__placeholder',
+        	selectTxt : '.js-align-placeholder',
         	selectOpt : '.support-select__options',
+        	selectOptLink : '.support-select__options a',
+        	manualWrap : '.manual-download-filter-new__module',
+        	manualList : '.manual-download-filter-new__content-list li',
+        	showClass : 'is-show',
+            resetBtn : '.s-btn-reset',
+            checkWrap : '.js-chkbox-wrap',
+            disabledClass : 'is-disabled',
+            checkedClass : 'is-checked',
+            filterWrap : '.manual-download-filter-new__list',
+            filterBtn : '.manual-download-filter-new__list-title',
+            filterList : '.manual-download-filter-new__list-items',
+            filterActiveClass : 'filter-active',
             viewType : null,
             resizeStart : null
         };
@@ -25,16 +41,38 @@
         setElements : function () {
         	this.personaWrap = this.obj.find(this.opts.personaWrap);
         	this.personaBx = this.obj.find(this.opts.personaBx);
-        	this.selectPlaceholder = this.obj.find(this.opts.selectPlaceholder);
+        	this.searchWrap = this.obj.find(this.opts.searchWrap);
+        	this.searchLabel = this.searchWrap.find('label');
+        	this.searchInput = this.searchWrap.find('input');
+        	this.inpClear = this.obj.find(this.opts.inpClear);
+        	this.selectBtn = this.obj.find(this.opts.selectBtn);
+        	this.selectTxt = this.obj.find(this.opts.selectTxt);
         	this.selectOpt = this.obj.find(this.opts.selectOpt);
+        	this.selectOptLink = this.obj.find(this.opts.selectOptLink);
+        	this.manualWrap = this.obj.find(this.opts.manualWrap);
+        	this.manualList = this.obj.find(this.opts.manualList);
+        	this.checkWrap = this.obj.find(this.opts.checkWrap);
+        	this.checkInput = this.checkWrap.find('input');
+        	this.filterWrap = this.obj.find(this.opts.filterWrap);
+        	this.filterBtn = this.obj.find(this.opts.filterBtn);
+        	this.filterList = this.obj.find(this.opts.filterList);
         },
         bindEvents : function () {
         	this.personaBx.on('mouseenter focusin mouseleave focusout', $.proxy(this.onHoverFunc, this));
-        	this.selectPlaceholder.on('click', $.proxy(this.onSelectFunc, this));
+        	this.selectBtn.on('click', $.proxy(this.onSelectEvt, this));
+            this.selectOptLink.on('click', $.proxy(this.onSelectOpt, this));
+            this.searchLabel.on('click', $.proxy(this.searchFunc, this));
+            this.searchInput.on('mouseenter focusin', $.proxy(this.searchFunc, this));
+            this.checkInput.on('change', $.proxy(this.checkFunc, this));
+            this.filterBtn.on('click', $.proxy(this.filterFunc, this));
         },
         setLayout : function () {
         	this.personaBx.removeClass(this.opts.activeClass);
         	this.selectOpt.hide();
+        	this.manualList.addClass(this.opts.showClass);
+        	this.checkWrap.removeClass(this.opts.checkedClass);
+        	this.filterWrap.removeClass(this.opts.filterActiveClass);
+        	this.filterList.hide();
         },
         onHoverFunc : function (e) {
         	var target = $(e.currentTarget);
@@ -45,9 +83,57 @@
     		} else if (e.type === 'mouseleave' || e.type === 'focusout') {
         		target.removeClass(this.opts.activeClass);
     		}
+    		// 선택 시 고정/리셋 버튼 활성화 추가
         },
-        onSelectFunc : function () {
+        searchFunc : function (e) {
+        	if (e.type === 'mouseenter' || e.type === 'focusin') {
+        		this.searchLabel.hide();
+        		this.inpClear.show();
+        	}
+        	// close 버튼 처리, 리셋 기능, 내용이 없을 경우 placeholder 활성화
+        },
+        checkFunc : function () {
+            if (this.checkWrap.find('input').filter(':checked')) {
+                this.checkWrap.toggleClass(this.opts.checkedClass);
+                console.log(this.checkWrap.prop('checked'));
+            }
+            // 각각 나누기
+        },
+        filterFunc : function (e) {
+        	e.preventDefault();
+        	var target = $(e.currentTarget);
+            this.currentIndex = target.parent().index();
+        	this.slideFunc();
+        },
+        slideFunc : function () {
+    		this.filterWrap.eq(this.currentIndex).toggleClass(this.opts.filterActiveClass);
+    		this.filterList.eq(this.currentIndex).slideToggle();
+            // 각각 나누기
+        },
+        onSelectEvt : function () {
         	this.selectOpt.show();
+            var selectOutsideTime = setTimeout($.proxy(function () {
+                this.onSelectOutside(true);
+            }, this), 30);
+        },
+        onSelectOpt :function (e) {
+            var targetOpt = e.currentTarget;
+            this.targetTxt = targetOpt.text;
+            this.selectTxt.text(this.targetTxt);
+            this.onSelectOutsideFunc();
+        },
+        onSelectOutside : function (type) {
+            if (type) {
+                this.selectOpt.on('clickoutside', $.proxy(this.onSelectOutsideFunc, this));
+            } else {
+                this.selectOpt.off('clickoutside');
+            }
+        },
+        onSelectOutsideFunc : function () {
+            this.selectOpt.hide();
+            var selectCloseTime = setTimeout($.proxy(function () {
+                this.onSelectOutside(false);
+            }, this), 30);
         }
     };
     $.fn.pluginCall = function () {
